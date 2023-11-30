93feaacdfb32ef5a4716fc6179123accab255e02
WindxFire <aeronredcelajes@gmail.com>
2023-11-30 12:29:44 +0800

    singletonPattern code here

--- singletonPattern/QueueManagementSystem.java
+++ singletonPattern/QueueManagementSystem.java
@@ -2,7 +2,7 @@ package singletonPattern;
 import java.util.ArrayList;
 import java.util.List;
 
-// Singleton Classe
+// Singleton Class
 public class QueueManagementSystem {
     private static QueueManagementSystem instance;
     private List<HelpDeskStation> helpDeskStations;
